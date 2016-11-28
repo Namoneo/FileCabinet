3272893665ba37edaf2917abe38ee062211fe6da
Namoneo <namoneo@icloud.com>
2016-11-28 21:35:19 +0100

    Devise, simple form, Docs controller and model

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,29 @@
+class DocsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def docs_params
+  end
+
+  def set_docs
+  end
+
+end
